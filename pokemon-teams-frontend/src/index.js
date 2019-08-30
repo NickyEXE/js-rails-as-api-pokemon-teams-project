29eabc61f4d58f89df64b63c3a1b708a8e08f3c6
Nicky_Dover.exe <dovern42@gmail.com>
2019-08-30 13:40:06 -0400

    Added index.js

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,35 @@
-const BASE_URL = "http://localhost:3000"
-const TRAINERS_URL = `${BASE_URL}/trainers`
-const POKEMONS_URL = `${BASE_URL}/pokemons`
+// Render methods
+
+// Converts fetch data to cards
+function handleTrainerFetchAsCards(data){
+    document.querySelector("main").innerHTML = data.map(trainer => trainerToCard(trainer)).join('')
+}
+// iterative method called above to convert an individual trainer to a card
+function trainerToCard(trainer){
+    return `<div class="card" data-id=${trainer.id}><p>${trainer.name}</p>
+    <button class="add" data-trainer-id=${trainer.id}>Add Pokemon</button>
+    <ul>` + trainer.pokemons.map(pokemon => pokemonToLi(pokemon)).join('') + `</ul>
+</div>`
+}
+
+// Called in each card for each pokemon to return the pokemon as a list
+function pokemonToLi(pokemon){
+    return `<li>${pokemon.nickname} (${pokemon.species}) <button class="release" data-pokemon-id=${pokemon.id}>Release</button></li>`
+}
+
+function handleCLick(e){
+    switch (e.target.classList[0]){
+        case "add":
+            adapter.addPokemon(e.target.dataset.trainerId)
+            break;
+        case "release":
+            adapter.destroyPokemon(e.target.dataset.pokemonId)
+            break;
+        default:
+            break;
+    }
+}
+
+// Initial execution functions: Populate the page and add an event listener.
+adapter.fetchTrainerData()
+document.querySelector("main").addEventListener('click', handleCLick)
